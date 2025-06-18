1e6c25e9695ce33fd2b87d34455961bd0e66fbe5
KD <itzkushan@gmail.com>
2025-06-18 14:17:17 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='info_retrieval_gen_ai',
+    version='0.0.0',
+    author='Kushan',
+    author_email='kushan@example.com',
+    packages=find_packages(),
+    install_requires=[]
+)
